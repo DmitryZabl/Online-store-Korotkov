37f0271bd1769783949c51228c32330b0d971a2e
OtetcSergy <zblot133@gmail.com>
2020-02-03 17:14:08 +0300

    without mnmkr2

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://korotkov-balzam.ru/admin/');
+define('HTTP_CATALOG', 'http://korotkov-balzam.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://korotkov-balzam.ru/admin/');
+define('HTTPS_CATALOG', 'http://korotkov-balzam.ru/');
+
+// DIR
+define('DIR_APPLICATION', 'W:/domains/korotkov-balzam.ru/admin/');
+define('DIR_SYSTEM', 'W:/domains/korotkov-balzam.ru/system/');
+define('DIR_IMAGE', 'W:/domains/korotkov-balzam.ru/image/');
+define('DIR_LANGUAGE', 'W:/domains/korotkov-balzam.ru/admin/language/');
+define('DIR_TEMPLATE', 'W:/domains/korotkov-balzam.ru/admin/view/template/');
+define('DIR_CONFIG', 'W:/domains/korotkov-balzam.ru/system/config/');
+define('DIR_CACHE', 'W:/domains/korotkov-balzam.ru/system/storage/cache/');
+define('DIR_DOWNLOAD', 'W:/domains/korotkov-balzam.ru/system/storage/download/');
+define('DIR_LOGS', 'W:/domains/korotkov-balzam.ru/system/storage/logs/');
+define('DIR_MODIFICATION', 'W:/domains/korotkov-balzam.ru/system/storage/modification/');
+define('DIR_UPLOAD', 'W:/domains/korotkov-balzam.ru/system/storage/upload/');
+define('DIR_CATALOG', 'W:/domains/korotkov-balzam.ru/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'admin1');
+define('DB_PASSWORD', 'admin2');
+define('DB_DATABASE', 'korotkov-balzam');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://korotkov-balzam.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://korotkov-balzam.ru/');
+
+// DIR
+define('DIR_APPLICATION', 'W:/domains/korotkov-balzam.ru/catalog/');
+define('DIR_SYSTEM', 'W:/domains/korotkov-balzam.ru/system/');
+define('DIR_IMAGE', 'W:/domains/korotkov-balzam.ru/image/');
+define('DIR_LANGUAGE', 'W:/domains/korotkov-balzam.ru/catalog/language/');
+define('DIR_TEMPLATE', 'W:/domains/korotkov-balzam.ru/catalog/view/theme/');
+define('DIR_CONFIG', 'W:/domains/korotkov-balzam.ru/system/config/');
+define('DIR_CACHE', 'W:/domains/korotkov-balzam.ru/system/storage/cache/');
+define('DIR_DOWNLOAD', 'W:/domains/korotkov-balzam.ru/system/storage/download/');
+define('DIR_LOGS', 'W:/domains/korotkov-balzam.ru/system/storage/logs/');
+define('DIR_MODIFICATION', 'W:/domains/korotkov-balzam.ru/system/storage/modification/');
+define('DIR_UPLOAD', 'W:/domains/korotkov-balzam.ru/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'admin1');
+define('DB_PASSWORD', 'admin2');
+define('DB_DATABASE', 'korotkov-balzam');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
