8b98d5b5e7411b5d4b49086251d31e70fed08d39
OtetcSergy <zblot133@gmail.com>
2020-03-16 21:16:26 +0300

    rec all

--- catalog/controller/extension/module/recommendation.php
+++ catalog/controller/extension/module/recommendation.php
@@ -10,22 +10,32 @@ class ControllerExtensionModuleRecommendation extends Controller {
 		$data['text_bone_and_joint'] = $this->language->get('text_bone_and_joint');
 		$data['text_cardio_vascular'] = $this->language->get('text_cardio_vascular');
 		$data['text_gastric_intestinal_parasitic'] = $this->language->get('text_gastric_intestinal_parasitic');
-		$data['text_LOR'] = $this->language->get('text_LOR');
-		$data['text_LOR'] = $this->language->get('text_LOR');
-		$data['text_LOR'] = $this->language->get('text_LOR');
-		$data['text_LOR'] = $this->language->get('text_LOR');
-		$data['text_LOR'] = $this->language->get('text_LOR');
+		$data['text_womens'] = $this->language->get('text_womens');
+		$data['text_mens'] = $this->language->get('text_mens');
+		$data['text_kidneys_and_urinary'] = $this->language->get('text_kidneys_and_urinary');
+		$data['text_nervous'] = $this->language->get('text_nervous');
+		$data['text_endocrine'] = $this->language->get('text_endocrine');
+		$data['text_muscle'] = $this->language->get('text_muscle');
+		$data['text_skin'] = $this->language->get('text_skin');
+		$data['text_veins'] = $this->language->get('text_veins');
+		$data['text_eye'] = $this->language->get('text_eye');
+		$data['text_HIV'] = $this->language->get('text_HIV');
 
 		$data['immunity'] = $this->url->link('information/information&information_id=8', '', true);
 		$data['LOR'] = $this->url->link('information/information&information_id=9', '', true);
 		$data['bone_and_joint'] = $this->url->link('information/information&information_id=10', '', true);
 		$data['cardio_vascular'] = $this->url->link('information/information&information_id=11', '', true);
 		$data['gastric_intestinal_parasitic'] = $this->url->link('information/information&information_id=12', '', true);
-		$data['LOR'] = $this->url->link('information/information&information_id=9', '', true);
-		$data['LOR'] = $this->url->link('information/information&information_id=9', '', true);
-		$data['LOR'] = $this->url->link('information/information&information_id=9', '', true);
-		$data['LOR'] = $this->url->link('information/information&information_id=9', '', true);
-		$data['LOR'] = $this->url->link('information/information&information_id=9', '', true);
+		$data['womens'] = $this->url->link('information/information&information_id=13', '', true);
+		$data['mens'] = $this->url->link('information/information&information_id=14', '', true);
+		$data['kidneys_and_urinary'] = $this->url->link('information/information&information_id=15', '', true);
+		$data['nervous'] = $this->url->link('information/information&information_id=16', '', true);
+		$data['endocrine'] = $this->url->link('information/information&information_id=17', '', true);
+		$data['muscle'] = $this->url->link('information/information&information_id=18', '', true);
+		$data['skin'] = $this->url->link('information/information&information_id=19', '', true);
+		$data['veins'] = $this->url->link('information/information&information_id=20', '', true);
+		$data['eye'] = $this->url->link('information/information&information_id=21', '', true);
+		$data['HIV'] = $this->url->link('information/information&information_id=22', '', true);
 
 		return $this->load->view('extension/module/recommendation', $data);
 	}
--- catalog/view/theme/moneymaker2/template/extension/module/recommendation.tpl
+++ catalog/view/theme/moneymaker2/template/extension/module/recommendation.tpl
@@ -1,14 +1,20 @@
-<div class="module-boxed">
+<div class="module-boxed" style=" margin-right: 15px;">
   <p class="h2"><?php echo $heading_title; ?></p>
-  <div class="list-group">
+  <div class="list-group" >
     <a href="<?php echo $immunity; ?>" class="list-group-item"><?php echo $text_immunity; ?></a>
     <a href="<?php echo $LOR; ?>" class="list-group-item"><?php echo $text_LOR; ?></a>
     <a href="<?php echo $bone_and_joint; ?>" class="list-group-item"><?php echo $text_bone_and_joint; ?></a>
     <a href="<?php echo $cardio_vascular; ?>" class="list-group-item"><?php echo $text_cardio_vascular; ?></a>
     <a href="<?php echo $gastric_intestinal_parasitic; ?>" class="list-group-item"><?php echo $text_gastric_intestinal_parasitic; ?></a>
-    <a href="<?php echo $LOR; ?>" class="list-group-item"><?php echo $text_LOR; ?></a>
-    <a href="<?php echo $LOR; ?>" class="list-group-item"><?php echo $text_LOR; ?></a>
-    <a href="<?php echo $LOR; ?>" class="list-group-item"><?php echo $text_LOR; ?></a>
-    <a href="<?php echo $LOR; ?>" class="list-group-item"><?php echo $text_LOR; ?></a>
+    <a href="<?php echo $womens; ?>" class="list-group-item"><?php echo $text_womens; ?></a>
+    <a href="<?php echo $mens; ?>" class="list-group-item"><?php echo $text_mens; ?></a>
+    <a href="<?php echo $kidneys_and_urinary; ?>" class="list-group-item"><?php echo $text_kidneys_and_urinary; ?></a>
+    <a href="<?php echo $nervous; ?>" class="list-group-item"><?php echo $text_nervous; ?></a>
+    <a href="<?php echo $endocrine; ?>" class="list-group-item"><?php echo $text_endocrine; ?></a>
+    <a href="<?php echo $muscle; ?>" class="list-group-item"><?php echo $text_muscle; ?></a>
+    <a href="<?php echo $skin; ?>" class="list-group-item"><?php echo $text_skin; ?></a>
+    <a href="<?php echo $veins; ?>" class="list-group-item"><?php echo $text_veins; ?></a>
+    <a href="<?php echo $eye; ?>" class="list-group-item"><?php echo $text_eye; ?></a>
+    <a href="<?php echo $HIV; ?>" class="list-group-item"><?php echo $text_HIV; ?></a>
   </div>
 </div>
