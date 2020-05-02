a2cae6578d74191d43f52b6ed729925f3553bebc
DmitryZabl <zblot133@gmail.com>
2020-05-02 09:08:00 +0300

    start FE

--- config.php
+++ config.php
@@ -6,23 +6,23 @@ define('HTTP_SERVER', 'http://korotkov-balzam.ru/');
 define('HTTPS_SERVER', 'http://korotkov-balzam.ru/');
 
 // DIR
-define('DIR_APPLICATION', 'W:/domains/korotkov-balzam.ru/catalog/');
-define('DIR_SYSTEM', 'W:/domains/korotkov-balzam.ru/system/');
-define('DIR_IMAGE', 'W:/domains/korotkov-balzam.ru/image/');
-define('DIR_LANGUAGE', 'W:/domains/korotkov-balzam.ru/catalog/language/');
-define('DIR_TEMPLATE', 'W:/domains/korotkov-balzam.ru/catalog/view/theme/');
-define('DIR_CONFIG', 'W:/domains/korotkov-balzam.ru/system/config/');
-define('DIR_CACHE', 'W:/domains/korotkov-balzam.ru/system/storage/cache/');
-define('DIR_DOWNLOAD', 'W:/domains/korotkov-balzam.ru/system/storage/download/');
-define('DIR_LOGS', 'W:/domains/korotkov-balzam.ru/system/storage/logs/');
-define('DIR_MODIFICATION', 'W:/domains/korotkov-balzam.ru/system/storage/modification/');
-define('DIR_UPLOAD', 'W:/domains/korotkov-balzam.ru/system/storage/upload/');
+define('DIR_APPLICATION', 'D:/OSPanel/domains/korotkov-balzam.ru/catalog/');
+define('DIR_SYSTEM', 'D:/OSPanel/domains/korotkov-balzam.ru/system/');
+define('DIR_IMAGE', 'D:/OSPanel/domains/korotkov-balzam.ru/image/');
+define('DIR_LANGUAGE', 'D:/OSPanel/domains/korotkov-balzam.ru/catalog/language/');
+define('DIR_TEMPLATE', 'D:/OSPanel/domains/korotkov-balzam.ru/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/OSPanel/domains/korotkov-balzam.ru/system/config/');
+define('DIR_CACHE', 'D:/OSPanel/domains/korotkov-balzam.ru/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/OSPanel/domains/korotkov-balzam.ru/system/storage/download/');
+define('DIR_LOGS', 'D:/OSPanel/domains/korotkov-balzam.ru/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/OSPanel/domains/korotkov-balzam.ru/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/OSPanel/domains/korotkov-balzam.ru/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'admin1');
+define('DB_USERNAME', 'f0416069_korotkov-balzam');
 define('DB_PASSWORD', 'q1w2e3');
-define('DB_DATABASE', 'korotkov-balzam');
+define('DB_DATABASE', 'f0416069_korotkov-balzam');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
